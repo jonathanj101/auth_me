1b2cf92c608bb12d9cfc416dadf74132a6a5f4b7
jonathanj101 <jonatjimen@gmail.com>
2024-03-17 20:59:47 -0400

    Project initialized

---
+++ server.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+
+const http = require("http");
+const app = require("./config/app");
+
+const db = require("./models/index");
+
+// initialize db connection, if it fails then don't listen to any requests!
+db();
+
+// initialize express server
+// console.log(+process.env.PORT);
+const port = +process.env.PORT || 8000;
+
+app.get("/", (req,res,next) => {
+    res.json({
+        expressServer: "Hellow World!"
+    })
+})
+
+// create server with the created express configurations
+const server = http.createServer(app);
+server.listen(port, () => {
+    // msg to know express server is running
+    console.log(`Express server listening on port ${port}`);
+})
